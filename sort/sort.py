5ec23cd1a9661b75fdbd04c3445c8e9998fdf272
huangshusheng <15820344259@163.com>
2020-10-10 01:14:42 +0800

    归并排序

--- sort/sort.py
+++ sort/sort.py
@@ -20,7 +20,11 @@ def bubblesort(arr):
             break
     return arr
 
+
 # 插入排序
+"""
+左边有序右边未排序
+"""
 
 
 def insertsort(arr):
@@ -51,14 +55,47 @@ def selectsort(arr):
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
+# 归并排序
+
+
+def mergesort(arr):
+    n = len(arr)
+    if n <= 1:
+        return arr
+    mid = int(n / 2)
+
+    def merge(left, right):
+        res, l_n, r_n = [], len(left), len(right)
+        i = j = 0
+        for _ in range(l_n+r_n):
+            if i >= l_n:
+                res.append(right[j])
+                j += 1
+            elif j >= r_n:
+                res.append(left[i])
+                i += 1
+            elif left[i] < right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+        return res
+
+    return merge(mergesort(arr[:mid]), mergesort(arr[mid:]))
+
+# 快速排序
+
 
 if __name__ == "__main__":
-    for i in range(10, 200, 1):
+    for i in range(10, 110, 1):
         arr = np.random.permutation(i)
         # bubble = bubblesort(arr.copy())
         # print(bubble)
-        #insert = insertsort(arr.copy())
+        # insert = insertsort(arr.copy())
         # print(insert)
-        select = selectsort(arr.copy())
-        print(select)
+        # select = selectsort(arr.copy())
+        # print(select)
+        merge = mergesort(arr.copy())
+        print(merge)
         # print(arr)
